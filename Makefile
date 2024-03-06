d7fe220fe650ce21270b65a3ebdb5449f7d93edb
piroot01 <piroot01@gmail.com>
2024-03-06 18:08:48 +0100

    v1

---
+++ Makefile
@@ -0,0 +1,12 @@
+CC = g++ 
+CFLAGS = -g -Wall -O3 --std=c++17
+
+TARGET = main
+
+all: $(TARGET)
+
+$(TARGET): $(TARGET).cpp
+	$(CC) $(CFLAGS) -o $(TARGET) $(TARGET).cpp
+
+clean:
+	$(RM) $(TARGET)
---
+++ main.cpp
@@ -0,0 +1,187 @@
+#include <cstdint>
+#include <cmath>
+#include <vector>
+#include <iostream>
+#include <limits>
+#include <iomanip>
+#include <numeric>
+#include <set>
+
+
+class Fraction
+{
+public:
+    Fraction(const double numerator = 1, const double denominator = 1) :
+        numerator(numerator), denominator(denominator)
+    {
+    }
+
+    bool operator==(Fraction& fraction)
+    {
+        fraction.reduce();
+        this->reduce();
+        return (this->numerator == fraction.numerator and this->denominator == fraction.denominator);
+    }
+
+    double getAsDouble() const
+    {
+        return (double)numerator / (double)denominator;
+    }
+
+    void reduce()
+    {
+        if (!reduced)
+        {
+            int32_t gcd = std::gcd(this->numerator, this->denominator);
+            this->numerator /= gcd;
+            this->denominator /= gcd;
+            reduced = true;
+        }
+    }
+
+    friend bool operator>(const Fraction& left, const Fraction& right)
+    {
+        int32_t c = left.numerator * right.denominator - left.denominator * right.numerator;
+        return (c > 0);
+    }
+
+    friend bool operator<(const Fraction& left, const Fraction& right)
+    {
+        int32_t c = left.numerator * right.denominator - left.denominator * right.numerator;
+        return (c < 0);
+    }
+
+    Fraction operator+(const Fraction& fraction) const
+    {
+        int32_t den = this->denominator * fraction.denominator;
+        return Fraction(this->numerator * fraction.denominator + fraction.numerator * this->denominator, den);
+    }
+
+    friend std::ostream& operator<<(std::ostream& stream, const Fraction& fraction)
+    {
+        stream << fraction.numerator << "/" << fraction.denominator;
+        return stream;
+    }
+
+public:
+    int32_t numerator;
+    int32_t denominator;
+    bool reduced = {false};
+
+};
+
+
+class Generator
+{
+public:
+    Generator(const Fraction& shift, const Fraction& period) :
+        m_shift(shift), m_period(period)
+    {
+    }
+
+    void assignInterval(const uint32_t start, const uint32_t end)
+    {
+        m_interval.first = Fraction(start, 1);
+        m_interval.second = Fraction(end, 1);
+    }
+
+    bool isInInterval(const Fraction& fraction) const
+    {
+        return fraction > m_interval.first and fraction < m_interval.second;
+    }
+
+    void generate(std::set<Fraction>& set)
+    {
+        // deduce start period
+        Fraction iter = m_shift;
+        while (iter < m_interval.first)
+            iter = iter + m_period;
+
+        iter.reduce();
+
+        //load the set
+        while (iter < m_interval.second)
+        {
+            iter = iter + m_period;
+            iter.reduce();
+            set.insert(iter);
+        }
+
+        uint32_t index = 0;
+        for (const auto& iter : set)
+            std::cout << ++index << " - " << iter << '\n';
+    }
+
+private:
+    Fraction m_shift;
+    Fraction m_period;
+
+    std::pair<Fraction, Fraction> m_interval;
+
+};
+
+
+class Result
+{
+public:
+    Result(const int32_t start, const int32_t end) :
+        m_intervalStart(start), m_intervalEnd(end)
+    {
+    }
+
+    void loadGenerator(const Generator& generator)
+    {
+        m_generators.push_back(generator);
+    }
+
+    void compute()
+    {
+        uint32_t index = 0;
+        for (auto& generator : m_generators)
+        {
+            std::cout << "generator [" << ++index << "]:\n";
+            generator.assignInterval(0, 12);
+            generator.generate(m_times);
+        }
+    }
+
+    void printResult() const
+    {
+        std::cout << m_times.size() << '\n';
+    }
+
+private:
+    std::vector<Generator> m_generators;
+    std::set<Fraction> m_times;
+    const int32_t m_intervalStart;
+    const int32_t m_intervalEnd;
+
+};
+
+
+int main()
+{
+    const int32_t basicPeriod = 12;
+
+    Generator gen0(Fraction((-1) * basicPeriod, 6 * 11), Fraction(basicPeriod, 12 - 1));
+    Generator gen1(Fraction(basicPeriod, 6 * 11), Fraction(basicPeriod, 12 - 1));
+    Generator gen2(Fraction(-basicPeriod, 6 * 719), Fraction(basicPeriod, 720 - 1));
+    Generator gen3(Fraction(basicPeriod, 6 * 719), Fraction(basicPeriod, 720 - 1));
+    Generator gen4(Fraction(-basicPeriod, 6 * 708), Fraction(basicPeriod, 720 - 12));
+    Generator gen5(Fraction(basicPeriod, 6 * 708), Fraction(basicPeriod, 720 - 12));
+
+    Result result(0, basicPeriod);
+    result.loadGenerator(gen0);
+    result.loadGenerator(gen1);
+    result.loadGenerator(gen2);
+    result.loadGenerator(gen3);
+    result.loadGenerator(gen4);
+    result.loadGenerator(gen5);
+
+    result.compute();
+    std::cout << "----------\n";
+    result.printResult();
+
+
+    return 0;
+}
